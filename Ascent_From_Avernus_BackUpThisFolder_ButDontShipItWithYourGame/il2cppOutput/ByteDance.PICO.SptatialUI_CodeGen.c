93f546eac4007121292d7e06f6d040dfa8abcf97
Hemang Mehra <mehra.hemang2023@gmail.com>
2026-07-18 12:19:07 -0700

    Fix for source control

---
+++ Ascent_From_Avernus_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/ByteDance.PICO.SptatialUI_CodeGen.c
@@ -0,0 +1,42 @@
+﻿#include "pch-c.h"
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+extern void UnitySourceGeneratedAssemblyMonoScriptTypes_v1_Get_mC3D1B1DF7C4BE9EF59DEC8A95ED0F826689048D0 (void);
+extern void UnitySourceGeneratedAssemblyMonoScriptTypes_v1__ctor_m1B2F71950D4C48BA2FD0E49C612D80C86987B8A6 (void);
+static Il2CppMethodPointer s_methodPointers[2] = 
+{
+	UnitySourceGeneratedAssemblyMonoScriptTypes_v1_Get_mC3D1B1DF7C4BE9EF59DEC8A95ED0F826689048D0,
+	UnitySourceGeneratedAssemblyMonoScriptTypes_v1__ctor_m1B2F71950D4C48BA2FD0E49C612D80C86987B8A6,
+};
+static const int32_t s_InvokerIndices[2] = 
+{
+	27966,
+	16221,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_ByteDance_PICO_SptatialUI_CodeGenModule;
+const Il2CppCodeGenModule g_ByteDance_PICO_SptatialUI_CodeGenModule = 
+{
+	"ByteDance.PICO.SptatialUI.dll",
+	2,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+};
